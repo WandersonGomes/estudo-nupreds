af09d70cc7471002eab22e5f44abbf20c4289a9b
WandersonGomes <dersom100@gmail.com>
2021-05-19 13:43:06 -0300

    Adicionar modulos usuarios e denuncias ao INSTALLED_APPS

--- projeto_zero/projeto_zero/settings.py
+++ projeto_zero/projeto_zero/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    ''
+    'usuarios.apps.UsuariosAppConfig',
+    'denuncias.apps.DenunciasAppConfig',
 ]
 
 MIDDLEWARE = [
