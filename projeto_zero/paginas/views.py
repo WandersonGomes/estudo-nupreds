db258ce89a5a1bb16b88b7ca3c2834a987eff7ba
WandersonGomes <dersom100@gmail.com>
2021-05-20 16:22:44 -0300

    Adiionr Sandbox

--- projeto_zero/paginas/views.py
+++ projeto_zero/paginas/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class PaginaInicialView(TemplateView):
-    template_name = "paginas/base.html"
+    template_name = "paginas/base-dashboard.html"
